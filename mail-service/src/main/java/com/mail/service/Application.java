e2a3248324a479b99dd3cdaf52f4a314c981a29d
Thomas <thomas.heinrichs@camunda.com>
2020-10-27 14:21:39 +0100

    Added External Task Worker and Postman Library

---
+++ mail-service/src/main/java/com/mail/service/Application.java
@@ -0,0 +1,59 @@
+package com.mail.service;
+
+import org.camunda.bpm.client.ExternalTaskClient;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.topic.TopicSubscriptionBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Level;
+
+@SpringBootApplication
+public class Application {
+
+	private static final String TOPIC = "mailservice";
+	private static final String CAMUNDAENDPOINT = "http://localhost:8080/engine-rest";
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class.getName());
+	private static String content = new String();
+	private static String recipient = new String();
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		ExternalTaskClient client = ExternalTaskClient.create()
+				.baseUrl(CAMUNDAENDPOINT)
+				.asyncResponseTimeout(10000)
+				.disableBackoffStrategy()
+				.maxTasks(1)
+				.build();
+
+		TopicSubscriptionBuilder subscriptionBuilder = client
+				.subscribe(TOPIC)
+				.lockDuration(20000)
+				.handler((externalTask, externalTaskService) -> {
+
+					createNotificationFromVariables(externalTask);
+					try {
+						LOGGER.info("Mail sent to: " + recipient);
+						LOGGER.info("Content: " + content);
+						externalTaskService.complete(externalTask);
+					} catch (Exception e) {
+						LOGGER.info((Marker) Level.SEVERE, "Exception occured", e);
+					}
+				});
+
+		client.start();
+		subscriptionBuilder.open();
+	}
+
+	private static void createNotificationFromVariables(ExternalTask externalTask){
+		try{
+			content = externalTask.getVariable("content");
+			recipient = externalTask.getVariable("recipient");
+		}catch (Exception exception){
+			LOGGER.info((Marker) Level.SEVERE, "Exception occured", exception);
+		}
+	}
+}
