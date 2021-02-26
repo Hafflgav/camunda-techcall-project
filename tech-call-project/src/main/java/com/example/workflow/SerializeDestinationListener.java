e9ab679043611ad5a5b906f1ff46b597e3faf48e
Thomas <thomas.heinrichs@camunda.com>
2021-02-26 14:35:49 +0100

    Added Logback

--- tech-call-project/src/main/java/com/example/workflow/SerializeDestinationListener.java
+++ tech-call-project/src/main/java/com/example/workflow/SerializeDestinationListener.java
@@ -3,13 +3,18 @@ package com.example.workflow;
 import com.example.pojo.Destination;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.ExecutionListener;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
 
 import static org.camunda.spin.Spin.JSON;
 
 
 @Component
 public class SerializeDestinationListener implements ExecutionListener {
+
+    private Logger logger = LoggerFactory.getLogger(SerializeDestinationListener.class.getName());
+
     @Override
     public void notify(DelegateExecution delegateExecution) throws Exception {
         Destination destination = JSON(delegateExecution.getVariable("destination")).mapTo(Destination.class);
@@ -17,5 +22,8 @@ public class SerializeDestinationListener implements ExecutionListener {
         delegateExecution.setVariable("state", destination.getState());
         delegateExecution.setVariable("city", destination.getCity());
         delegateExecution.setVariable("address", destination.getStreetAndNumber());
+
+        logger.info("Set destination to" + destination.getCountry() + ", " + destination.getState() +
+                ", " + destination.getCity() + ", " + destination.getStreetAndNumber());
     }
 }
